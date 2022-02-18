0068fc0c2c8cfdef258c42f8d534a8f72e8cf78b
River <75388756+RK-MSU@users.noreply.github.com>
2022-02-17 17:45:16 -0700

    Working Right Hand Rule (#9)
    * Assignment Instructions (#1)
    
    Co-authored-by: River Kelly <65210753+RK311y@users.noreply.github.com>
    
    * Ignore peksy macOS files
    
    * init given assignment files
    
    * Fix water flow textures (#3)
    
    Co-authored-by: River Kelly <65210753+RK311y@users.noreply.github.com>
    
    * Add e-puck robot to maze (#4)
    
    * Add e-puck robot to masze
    
    * Set robot controller to <extern>
    
    * Move robot to start position
    
    * Convert robot to base root nodes
    
    * Add compass to robot
    
    * Add touch sensor to robot
    
    * Adjust touch sensor and add specs information
    
    Co-authored-by: River Kelly <65210753+RK311y@users.noreply.github.com>
    
    * Fix/touch sensor (#5)
    
    * init main.py
    
    * Add title and END (of file) comment
    
    * Add EPuck class and base code
    
    * Fix Touch sensor
    
    Co-authored-by: River Kelly <65210753+RK311y@users.noreply.github.com>
    
    * Setup robot extern controller (#6)
    
    * init main.py
    
    * Add title and END (of file) comment
    
    * Add EPuck class and base code
    
    Co-authored-by: River Kelly <65210753+RK311y@users.noreply.github.com>
    
    * Update .gitignore - Local IDE Files
    
    - .vscode/
    
    - .idea/
    
    * Update README.md
    
    * Update controller
    
    * Update Maze - Fixes solid 18 boundingObject Box (#7)
    
    Co-authored-by: River Kelly <65210753+RK311y@users.noreply.github.com>
    
    * Touch trophy end game (#8)
    
    * Add code to check touch sensor
    
    * Update touch sensor position
    
    Co-authored-by: River Kelly <65210753+RK311y@users.noreply.github.com>
    
    Co-authored-by: River Kelly <65210753+RK311y@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,104 @@
+# Robot Maze - Find the Trophy
+
+> CSCI-455 Embedded Systems: Robotics
+
+- [Instructions](#instructions)
+  - [Maze Escape](#maze-escape)
+  - [What to Do](#what-to-do)
+  - [Setup](#setup)
+  - [Grading](#grading)
+  - [How We will Grade](#how-we-will-grade)
+  - [Customization of the robot or playing field](#customization-of-the-robot-or-playing-field)
+
+## E-Puck Robot
+
+### Specs
+
+#### Touch Sensor
+
+- Translation:  0 0.011
+  - `x=0.0363`
+  - `y=0`
+  - `z=0.011`
+- boundingObject `Box`
+  - `x=0.0025`
+  - `y=0.02`
+  - `z=0.02`
+
+## Instructions
+
+### Maze Escape
+
+I will admit this is a little tricky.  I've completed all the steps. I just need to dial in the speeds for optimization. 
+
+I gave you everything you need to complete the (mouse finding the cheese) maze assignment with the videos for the past two weeks. Encoders, compass, motors, touch, ePuck, etc. etc. 
+
+You will learn the robot is very moody. 
+
+The World you need is the next link on D2L.  The world I built comes with a maze, ePuck, and target. You will need to program the robot to find the trophy. Program the robot to find the trophy no matter where the robot starts (we will use a default starting point) or where the trophy is moved too in the maze. (No hard coding the path, has to be autonomously explored).
+
+In class, Monday, after the program is due we will have some races,. Two mazes next to each other, place the robot and finish the same location in both mazes and see who can finish first over 3 runs.  
+
+### What to Do
+
+You are to write source code in Python or Java to get your ePuck robot to make it out of the maze in the shortest amount of time. 
+
+You can run the maze **three times**, the first two are to learn the maze, the **third time to get the best time**.  After the third time all your maze data must be erased(delete your data file) and the maze resets. 
+
+### Setup
+
+For the assignment grading your robot will always start in the same spot.
+
+- `x = -0.04 to -.06`
+- `y = -0.6`
+- rotation, if you need to start a tiny bit rotated and a little closer to one wall you can. 
+- `max speed = 6.28` (I did not control it well at this speed, I slowed it down a bit)
+- `step = 64`
+
+### Grading
+
+You get three tries, the first try your robot knows nothing about the maze, the second and third run you can use what your robot has learned about the maze. You are not allowed to change any code between all steps to get the points. You can't change the code between any number below to get the points. Only the location of the goal will change.  You can only hit-run like it's the power button on the robot.  Each goal will have a time limit which is in the parenthesis before the points on each level, so you can't just brute force for thirty minutes to find the goal. The first few you should be able to get just doing one run.  
+
+You can use a data file to save the stuff you learned, load the data file from memory for runs two and three,  but after three runs the data file will be deleted and you start over from scratch. 
+
+There should be no imaginary walls built into your code on run one, you know nothing about the maze. 
+
+_**ONE SOURCE CODE FILE**_ should be able to find all the locations below under the time limits within the three runs you can take. 
+
+1. **20 points** will be based on the quality of your source code, good object-oriented Python or Java code. If you have been using C you will need to change it to Python or Java.  I have not been doing OO in class, but my Python solution now has good OO programming. It makes it easier. 
+2. (0:12:40 seconds) **20 points** your robot can find and detect the trophy at `x=-0.06`, `y=0`, when detected your code should exit and the Webots clock should stop. 
+3. (0:27:00 seconds ) **10 points** your robot can find and detect the trophy at `x=0.54`, `y=-0.14`.
+4. (0:56:30 ) **10 points** your robot can find and detect the trophy at `x=0.66`, `z=-66`. _(bottom right corner)_
+5. (0:26:30 ) **10 points** your robot can find and detect the trophy at the bottom left corner `x=-.66`, `y=-.66`. You have to use the exact same code as in the three levels above, you would have to find the positions above in run one, and this position in run two or possibly three. 
+6. (0:14:00 ) **10 points** your robot can find and detect the trophy at `x=-0.66`, `y=0.4` and not take the path that leads you to where the trophy in #5 above was located. So you try path where #5 was located on run one or two, and then on the next run, you learn that is a dead-end.  
+7. (0:39:00 ) **10 points** the default trophy location `x=-0.48`, `y=0.66`
+8. (0:35:46 ) **10 points** `x=-0.48`, `z=0.66` default trophy location but your path is optimal
+
+My Record: I gave you guys a `1.44x1.44` field. Left-hand rule only record I did in 23 seconds - FASTEST TIME using a combo of left hand rule and right hand rule: 
+
+Record on a `1.44x1.44` is `0:20:48` (pic below)
+
+### How we will grade
+
+First, we will put the trophy in the coordinates from #2 and make sure you can detect the trophy with your robot (print out "Win") when you hit the trophy and exit out of your code. That will give you the 20 points for #2
+
+Then we will delete your data file, if you are using one and start from scratch. We will move the trophy to the default position. Then run your program and watch it go through all the coordinates above in the time limits and give points as your robot performs. When all the time limits are done we will stop run #1.  
+
+We will then start your program again for run two....add any more points you get if you hit coordinates under time limits. 
+
+Then we will do the same thing for run three. 
+
+We will only delete a data file, we will not change any code, run a second file or create a data file. 
+
+We will only check the robot detecting the trophy at the location of #2 and the default location in the last couple of steps. The rest of the coordinates listed above your robot will only have to drive over to get the points. 
+
+### Customization of the robot or playing field
+
+You can not customize the playing field or trophy, these should stay as the default world you were given, except the fact you will translate the trophy around the field. 
+
+The robot has to be the ePuck everything must be default except for the following, you can add two items:
+
+1. A compass keep the default parameters
+2. A touch sensor to detect the trophy. You can customize where the touch sensor is located, but you can not move the sensor off the robot to the degree it is not traveling closely with the robot. In other words, you can't start the robot at the default location and the sensor is located at the trophy. It must be simulated as connected to the robot, slightly in front, right or left is fine.  You will need to let us know the coordinates of your touch sensor, that is the only thing we will do to our robot before we run it. (Our robot already has the compass and touch sensor on it).
+
+![Hunters Screenshot](images/hunters-maze-screen-shot.png)
---
+++ main.py
@@ -0,0 +1,274 @@
+'''
+River Kelly
+Kyler Gappa
+CSCI-455: Embedded Systems (Robotics)
+Assignment 01 - Webots E-Puck Robot Maze
+Spring 2022
+'''
+
+from controller import Robot
+from enum import Enum
+import math
+
+TIME_STEP = 64
+MAX_SPEED = 6.28
+
+# --------------------------------------------------------------------
+
+# Direction Class
+class Direction(Enum):
+    North = 'North'
+    South = 'South'
+    East = 'East'
+    West = 'West'
+    Left = 'Left'
+    Right = 'Right'
+
+# RobotState Class
+class RobotState(Enum):
+    FindWall = 'Find Wall'
+    MountWall = 'Mount Wall'
+    FollowWall = 'Follow Wall'
+    TurnCorner = 'Turn Corner'
+    CorrectTurn = 'CorrectTurn'
+    GameOver = 'Game Over'
+
+# --------------------------------------------------------------------
+
+# EPuck (Robot) Class
+class EPuck:
+
+    # properties
+    robot = None
+    ps = []
+    leftMotor = None
+    rightMotor = None
+    leftMotorSensor = None
+    rightMotorSensor = None
+    compass = None
+    touchSensor = None
+
+    _state = RobotState.FindWall
+
+    # constructor
+    def __init__(self, robot):
+        self.robot = robot
+        # get and enable ultrasound sensors
+        for i in range(8):
+            sensorName = "ps{!s}".format(i)
+            sensor = self.robot.getDevice(sensorName)
+            sensor.enable(TIME_STEP)
+            self.ps.append(sensor)
+        # Setup left and right motors
+        self.leftMotor = self.robot.getDevice('left wheel motor')
+        self.rightMotor = self.robot.getDevice('right wheel motor')
+        self.leftMotor.setPosition(float('inf'))
+        self.rightMotor.setPosition(float('inf'))
+        self.leftMotor.setVelocity(0.0)
+        self.rightMotor.setVelocity(0.0)
+        # wheel sensors
+        self.leftMotorSensor = self.robot.getDevice('left wheel sensor')
+        self.rightMotorSensor = self.robot.getDevice('left wheel sensor')
+        self.leftMotorSensor.enable(TIME_STEP)
+        self.rightMotorSensor.enable(TIME_STEP)
+        # compass
+        self.compass = self.robot.getDevice('compass')
+        self.compass.enable(TIME_STEP)
+        # touch sensor
+        self.touchSensor = self.robot.getDevice('touch sensor')
+        self.touchSensor.enable(TIME_STEP)
+
+    # step()
+    # ----------------------------------------------------------------
+    # TODO: add description
+    def step(self):
+        if self.robot.step(TIME_STEP) != -1:
+            return True
+        return False
+
+    # bearing (property)
+    # ----------------------------------------------------------------
+    # Returns the degree (0-360) which the robot is facing
+    @property
+    def bearing(self):
+        dir = self.compass.getValues()
+        if math.isnan(dir[0]):
+            return None
+        rad = math.atan2(dir[0], dir[1])
+        bearing = (rad - 1.5708) / math.pi * 180.0
+        if bearing < 0.0:
+            bearing = bearing + 360.0
+        return bearing
+
+    # direction (property)
+    # ----------------------------------------------------------------
+    # returns the compass pole (North, South, East, or West) which the
+    # robot is currently facing basesd on the bearing
+    @property
+    def direction(self):
+        bearing = self.bearing
+        if bearing is None:
+            return None
+        if 45 < bearing <= 135:
+            return Direction.West
+        elif 135 < bearing <= 225:
+            return Direction.South
+        elif 225 < bearing <= 315:
+            return Direction.East
+        else:
+            return Direction.North
+
+    # setSpeed()
+    # ----------------------------------------------------------------
+    # Set the robots wheel speed based on a percentage of the
+    # MAX_SPEED.
+    #
+    # Example: self.setSpeed(50)
+    # sets wheel velocity to 50% of the MAX_SPEED
+    # ----------------------------------------------------------------
+    def setSpeed(self, speed):
+        speed = MAX_SPEED * (speed / 100)
+        self.leftMotor.setVelocity(speed)
+        self.rightMotor.setVelocity(speed)
+
+    # setLeftWheelSpeed()
+    def setLeftWheelSpeed(self, speed):
+        speed = MAX_SPEED * (speed / 100)
+        self.leftMotor.setVelocity(speed)
+
+    # setRightWheelSpeed()
+    def setRightWheelSpeed(self, speed):
+        speed = MAX_SPEED * (speed / 100)
+        self.rightMotor.setVelocity(speed)
+
+    def printSensorValues(self):
+        
+        front_left = self.ps[7].getValue()
+        front_right = self.ps[0].getValue()
+        corner_right = self.ps[1].getValue()
+        corner_left = self.ps[6].getValue()
+        side_right = self.ps[2].getValue()
+        side_left = self.ps[5].getValue()
+        back_right = self.ps[3].getValue()
+        back_left = self.ps[4].getValue()
+
+        TBL_END = "-------------------------------------------------------------------------------"
+        TBL_SPACER = "|{!s:-^8}|{!s:-^8}|{!s:-^10}|{!s:-^9}|{!s:-^9}|{!s:-^10}|{!s:-^8}|{!s:-^8}|"
+        TBL_HEADER = "|{!s:^8}|{!s:^8}|{!s:^10}|{!s:^9}|{!s:^9}|{!s:^10}|{!s:^8}|{!s:^8}|"
+        TBL_ROW = "|{:^8.1f}|{:^8.1f}|{:^10.1f}|{:^9.1f}|{:^9.1f}|{:^10.1f}|{:^8.1f}|{:^8.1f}|"
+        TABLE_STR = TBL_END + "\n" +TBL_HEADER + "\n" + TBL_SPACER + "\n" + TBL_ROW + "\n" + TBL_END + "\n"
+        
+        STR = TABLE_STR.format(
+            "L-Back", "L-Side", "L-Corner", "L-Front", "R-Front", "R-Corner", "R-Side", "R-Back",
+            '', '', '', '', '', '', '', '',
+            back_left, side_left, corner_left, front_left, front_right, corner_right, side_right, back_right)
+        
+        print(STR)
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, state):
+        print("Switching RobotState:\n---------------------\n To: '{!s}' - From: '{!s}'\n".format(state.value, self.state.value))
+        self._state = state
+
+
+    # travel()
+    # ----------------------------------------------------------------
+    # TODO: add description
+    def travel(self):
+        front_left = self.ps[7].getValue()
+        front_right = self.ps[0].getValue()
+        corner_right = self.ps[1].getValue()
+        corner_left = self.ps[6].getValue()
+        side_right = self.ps[2].getValue()
+        side_left = self.ps[5].getValue()
+        back_right = self.ps[3].getValue()
+        back_left = self.ps[4].getValue()
+
+        # Game Over?
+        touch = self.touchSensor.getValue()
+        if not math.isnan(touch) and touch > 0 and front_left < 80 and front_right < 80:
+            self.state = RobotState.GameOver
+            return
+
+        # print(self.state.name)
+        # self.printSensorValues()
+
+        # ------------------------------------------------------------
+        # Find Wall
+        # ------------------------------------------------------------
+        if self.state == RobotState.FindWall:
+            self.setSpeed(50)
+            if front_left > 200 and front_right > 200:
+                self.setSpeed(0)
+                self.state = RobotState.MountWall
+            elif front_left > 150 and front_right > 150:
+                self.setSpeed(5)
+            elif front_left > 80 and front_right > 80:
+                self.setSpeed(20)
+        # ------------------------------------------------------------
+        # Mount Wall
+        # ------------------------------------------------------------
+        elif self.state == RobotState.MountWall:
+            self.setLeftWheelSpeed(-35)
+            self.setRightWheelSpeed(35)
+            if front_left > 80:
+                return
+            if corner_right < 120 and side_right > 100:
+                self.setSpeed(0)
+                self.state = RobotState.FollowWall
+        # ------------------------------------------------------------
+        # Follow Wall
+        # ------------------------------------------------------------
+        elif self.state == RobotState.FollowWall:
+            self.setSpeed(100)
+            # Facing Wall
+            if front_left > 100 and front_right > 100:
+                self.setSpeed(0)
+                self.state = RobotState.MountWall
+            # Turn Corner
+            elif side_right < 80 and corner_right < 80:
+                self.setSpeed(50) # TODO: do we need this?
+                self.state = RobotState.TurnCorner
+            # Adjust Left (more severly)
+            elif side_right > 200 and corner_right > 150:
+                self.setLeftWheelSpeed(-10)
+                self.setRightWheelSpeed(50)
+            # Adjust Left (moderate)
+            elif side_right > 200 and corner_right > 100:
+                self.setLeftWheelSpeed(80)
+                self.setRightWheelSpeed(100)
+            # Adjust Right
+            elif side_right < 150 and corner_right < 80:
+                self.setLeftWheelSpeed(100)
+                self.setRightWheelSpeed(80)
+        # ------------------------------------------------------------
+        # Turn Corner
+        # ------------------------------------------------------------
+        elif self.state == RobotState.TurnCorner:
+            self.setLeftWheelSpeed(50)
+            self.setRightWheelSpeed(8)
+            # Facing Wall
+            if front_left > 100 and front_right > 100:
+                self.setSpeed(0)
+                self.state = RobotState.MountWall
+            elif corner_right > 80:
+                self.setSpeed(5) # TODO: do we need this?
+                self.state = RobotState.FollowWall
+# END EPuck Class
+
+# trans:
+# rotation:
+
+print("Starting")
+robot = EPuck(Robot())
+while robot.step():
+    robot.travel()
+    if robot.state == RobotState.GameOver:
+        break
+print("GAME OVER")
+
+# END
